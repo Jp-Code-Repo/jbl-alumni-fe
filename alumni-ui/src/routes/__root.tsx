29cfa5c902e74fc026e16c259c519fee1b0eb038
Jp-Code-Repo <jpbuzz6100@gmail.com>
2026-06-29 11:03:13 +0800

    Full implementation of TanStack Router

--- alumni-ui/src/routes/__root.tsx
+++ alumni-ui/src/routes/__root.tsx
@@ -1,9 +1,15 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 
+import AppLayout from "@/app/layouts/AppLayout";
+
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
-  return <Outlet />;
+  return (
+    <AppLayout>
+      <Outlet />
+    </AppLayout>
+  );
 }
